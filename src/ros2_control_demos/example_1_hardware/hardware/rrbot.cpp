4579cc8686732bd2184c75644fb88eb191119094
Chong Liu <chongliu2021@163.com>
2025-05-16 20:01:11 +0800

    指定 rrbot.hpp

--- src/ros2_control_demos/example_1_hardware/hardware/rrbot.cpp
+++ src/ros2_control_demos/example_1_hardware/hardware/rrbot.cpp
@@ -1,6 +1,27 @@
 #include "rrbot.hpp"
 
+#include <chrono>
+#include <cmath>
+#include <iomanip>
+#include <limits>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "rclcpp/rclcpp.hpp"
+
 namespace rrbot_hardware
 {
+  hardware_interface::CallbackReturn RRBotHardware::on_init(const hardware_interface::HardwareInfo & info)
+  {
+    if (hardware_interface::SystemInterface::on_init(info) != hardware_interface::CallbackReturn::SUCCESS)
+    {
+        return hardware_interface::CallbackReturn::ERROR;
+    }
+    return hardware_interface::CallbackReturn::SUCCESS;
+  }
 
-}
+
+}  // namespace rrbot_hardware
