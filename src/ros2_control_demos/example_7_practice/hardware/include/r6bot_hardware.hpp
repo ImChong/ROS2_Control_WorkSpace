2c34dd5dd5f6c4046a08db4426a9d40896b4a7a4
Chong Liu <chongliu2021@163.com>
2025-05-20 01:32:05 +0800

    r6bot_hardware.hpp

--- src/ros2_control_demos/example_7_practice/hardware/include/r6bot_hardware.hpp
+++ src/ros2_control_demos/example_7_practice/hardware/include/r6bot_hardware.hpp
@@ -1,18 +1,18 @@
 #ifndef R6BOT_HARDWARE_HPP_
 #define R6BOT_HARDWARE_HPP_
 
-#include <memory>
-#include <string>
-#include <vector>
+#include "vector"
+#include "string"
+#include "unordered_map"
 
 #include "hardware_interface/handle.hpp"
 #include "hardware_interface/hardware_info.hpp"
 #include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
-#include "rclcpp/clock.hpp"
-#include "rclcpp/logger.hpp"
-#include "rclcpp/macros.hpp"
-#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+
+using hardware_interface::return_type;   // 返回类型
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;  // 回调返回类型
 
 namespace r6bot_hardware_namespace
 {
@@ -20,27 +20,24 @@ namespace r6bot_hardware_namespace
 class R6BotHardware : public hardware_interface::SystemInterface
 {
 public:
-  RCLCPP_SHARED_PTR_DEFINITIONS(R6BotHardware)
-
-  hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
+  CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
-  hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
-  hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
-  hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
-  hardware_interface::return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
-  hardware_interface::return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;
-  rclcpp::Logger get_logger() const { return *logger_; }
-  rclcpp::Clock::SharedPtr get_clock() const { return clock_; }
+  return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
+  return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
-private:
+protected:
   hardware_interface::HardwareInfo hardware_info_;   // 硬件信息
 
-  std::shared_ptr<rclcpp::Logger> logger_;            // 日志
-  rclcpp::Clock::SharedPtr clock_;                    // 时钟
+  std::vector<double> joint_position_command_;  // 关节位置命令
+  std::vector<double> joint_velocities_command_;  // 关节速度命令
+  std::vector<double> joint_position_;  // 关节位置
+  std::vector<double> joint_velocities_;  // 关节速度
+  std::vector<double> ft_states_;  // 力传感器状态
+  std::vector<double> ft_command_;  // 力传感器命令
 
-  std::vector<double> hw_states_;            // 状态
-  std::vector<double> hw_commands_;          // 命令
+  std::unordered_map<std::string, std::vector<std::string>> joint_interfaces = {
+    {"position", {}}, {"velocity", {}}};
 };
 
 } // namespace r6bot_hardware_namespace
