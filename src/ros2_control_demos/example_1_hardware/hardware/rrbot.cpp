d4f419dc1b03009516331f1875b480f9c968568e
Chong Liu <chongliu2021@163.com>
2025-05-19 12:09:08 +0800

    初始化硬件接口

--- src/ros2_control_demos/example_1_hardware/hardware/rrbot.cpp
+++ src/ros2_control_demos/example_1_hardware/hardware/rrbot.cpp
@@ -25,11 +25,35 @@ namespace rrbot_hardware
     logger_ = std::make_shared<rclcpp::Logger>(rclcpp::get_logger("RRBot 硬件消息"));  // 初始化日志
     clock_ = std::make_shared<rclcpp::Clock>(rclcpp::Clock());  // 初始化时钟
 
+    std::cout << "===== 初始化硬件接口 =====" << std::endl;
     // 打印所有info
-    std::cout << "info_.name: " << info_.name << std::endl;
-    std::cout << "info_.type: " << info_.type << std::endl;
-    std::cout << "info_.hardware_class_type: " << info_.hardware_class_type << std::endl;
-    std::cout << "info_.joints.size(): " << info_.joints.size() << std::endl;
+    std::cout << "info_.name[名称]: " << info_.name << std::endl;
+    std::cout << "info_.type[类型]: " << info_.type << std::endl;
+    std::cout << "info_.hardware_class_type[硬件类型]: " << info_.hardware_class_type << std::endl;
+    std::cout << "info_.joints.size()[关节数量]: " << info_.joints.size() << std::endl;
+
+    // 打印所有joints信息
+    std::cout << "info_.joints[关节列表]: " << std::endl;
+    for (const auto & joint : info_.joints)
+    {
+      std::cout << "\tjoint.name[关节名称]: " << joint.name << std::endl;
+      std::cout << "\tjoint.type[关节类型]: " << joint.type << std::endl;
+      std::cout << "\tjoint.command_interfaces[命令接口]: " << std::endl;
+      for (const auto & command_interface : joint.command_interfaces)
+      {
+        std::cout << "\t\tcommand_interface.name[接口名称]: " << command_interface.name << std::endl;
+        std::cout << "\t\tcommand_interface.data_type[接口数据类型]: " << command_interface.data_type << std::endl;
+        std::cout << "\t\tcommand_interface.size[接口大小]: " << command_interface.size << std::endl;
+      }
+      std::cout << "\tjoint.state_interfaces[状态接口]: " << std::endl;
+      for (const auto & state_interface : joint.state_interfaces)
+      {
+        std::cout << "\t\tstate_interface.name[接口名称]: " << state_interface.name << std::endl;
+        std::cout << "\t\tstate_interface.data_type[接口数据类型]: " << state_interface.data_type << std::endl;
+        std::cout << "\t\tstate_interface.size[接口大小]: " << state_interface.size << std::endl;
+      }
+    }
+    std::cout << "===== 初始化硬件接口结束 =====" << std::endl;
 
 
     return hardware_interface::CallbackReturn::SUCCESS;
