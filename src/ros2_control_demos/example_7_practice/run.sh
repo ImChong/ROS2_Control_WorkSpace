1e47c01ceafe5d40ee4b8a84a193958cad204fc1
Chong Liu <chongliu2021@163.com>
2025-05-27 14:52:18 +0800

    Update run.sh

--- src/ros2_control_demos/example_7_practice/run.sh
+++ src/ros2_control_demos/example_7_practice/run.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 set -e
 
-# 清理
 echo "===== 清理 build/ install/ log/ 目录 ====="
 rm -rf build/ install/ log/
 
-# 编译
 echo "===== 编译 ====="
 colcon build --packages-select r6bot_control_system
 
-# 设置环境变量
 echo "===== 设置环境变量 ====="
 source install/setup.bash
 
-# 启动
 echo "===== 启动 ====="
-ros2 launch r6bot_control_system view_r6bot.launch.py
-# ros2 launch r6bot_control_system r6bot_control_system.launch.py
-
+# ros2 launch r6bot_control_system view_r6bot.launch.py
+ros2 launch r6bot_control_system r6bot_control_system.launch.py
 
 echo "===== 结束 ====="
