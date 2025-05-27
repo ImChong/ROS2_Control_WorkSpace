085166dd9df2465e0215b1c3a9f8dd05ef0cbb07
Chong Liu <chongliu2021@163.com>
2025-05-27 14:36:41 +0800

    r6bot_control_system.launch.py

--- src/ros2_control_demos/example_7_practice/launch/r6bot_control_system.launch.py
+++ src/ros2_control_demos/example_7_practice/launch/r6bot_control_system.launch.py
@@ -1,4 +1,34 @@
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+
+    ####################################################################################################################
+    # 获取urdf文件
+    ####################################################################################################################
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("r6bot_control_system"),
+                    "description",
+                    "urdf",
+                    "r6bot.urdf.xacro",
+                ]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("r6bot_control_system"),
+            "r6bot_controller.yaml",
+        ]
+    )
+
     return
