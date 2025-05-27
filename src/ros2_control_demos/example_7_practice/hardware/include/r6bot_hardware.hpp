b2044c1cd6ee656f38c9680b91dea22ec3fa0823
Chong Liu <chongliu2021@163.com>
2025-05-27 15:11:50 +0800

    Update r6bot_hardware.hpp

--- src/ros2_control_demos/example_7_practice/hardware/include/r6bot_hardware.hpp
+++ src/ros2_control_demos/example_7_practice/hardware/include/r6bot_hardware.hpp
@@ -11,32 +11,30 @@
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
 #include "hardware_interface/types/hardware_interface_type_values.hpp"
 
-using hardware_interface::return_type;   // 返回类型
-using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;  // 回调返回类型
-
 namespace r6bot_hardware_namespace
 {
 
 class R6BotHardware : public hardware_interface::SystemInterface
 {
 public:
-  CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
-  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
-  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
-  return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
-  return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;
+  hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;  // 初始化硬件接口
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;  // 导出状态接口
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;  // 导出命令接口
+  hardware_interface::return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;  // 读取硬件状态
+  hardware_interface::return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;  // 写入硬件命令
 
 protected:
   hardware_interface::HardwareInfo hardware_info_;   // 硬件信息
 
+  std::vector<double> joint_position_states_;  // 关节位置状态
+  std::vector<double> joint_velocities_states_;  // 关节速度状态
+  std::vector<double> ft_states_;  // 力矩传感器状态
+
   std::vector<double> joint_position_command_;  // 关节位置命令
   std::vector<double> joint_velocities_command_;  // 关节速度命令
-  std::vector<double> joint_position_;  // 关节位置
-  std::vector<double> joint_velocities_;  // 关节速度
-  std::vector<double> ft_states_;  // 力传感器状态
-  std::vector<double> ft_command_;  // 力传感器命令
+  std::vector<double> ft_command_;  // 力矩传感器命令
 
-  std::unordered_map<std::string, std::vector<std::string>> joint_interfaces = {
+  std::unordered_map<std::string, std::vector<std::string>> joint_interfaces = {  // 关节接口
     {"position", {}}, {"velocity", {}}};
 };
 
