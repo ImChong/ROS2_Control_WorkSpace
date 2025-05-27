5ce199292c2eb7589b062380f5c450ca783f7a80
Chong Liu <chongliu2021@163.com>
2025-05-27 14:45:18 +0800

    Update view_r6bot.launch.py

--- src/ros2_control_demos/example_7_practice/launch/view_r6bot.launch.py
+++ src/ros2_control_demos/example_7_practice/launch/view_r6bot.launch.py
@@ -5,7 +5,18 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    # Get URDF via xacro
+    ####################################################################################################################
+    # 获取关节状态发布节点
+    ####################################################################################################################
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+    )
+
+    ####################################################################################################################
+    # 获取机器人状态发布节点
+    ####################################################################################################################
+    # 获取urdf文件
     robot_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
@@ -20,22 +31,27 @@ def generate_launch_description():
             ),
         ]
     )
+
+    # 将urdf文件转换为robot_description
     robot_description = {"robot_description": robot_description_content}
 
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("r6bot_control_system"), "description", "rviz", "view_r6bot.rviz"]
-    )
-
-    joint_state_publisher_node = Node(
-        package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui",
-    )
+    # 发布机器人状态
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="both",
         parameters=[robot_description],
     )
+
+    ####################################################################################################################
+    # 获取rviz节点
+    ####################################################################################################################
+    # 获取rviz配置文件
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("r6bot_control_system"), "description", "rviz", "view_r6bot.rviz"]
+    )
+
+    # 启动rviz
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
@@ -44,6 +60,10 @@ def generate_launch_description():
         arguments=["-d", rviz_config_file],
     )
 
+    ####################################################################################################################
+    # 获取节点
+    ####################################################################################################################
+    # 启动节点
     nodes = [
         joint_state_publisher_node,
         robot_state_publisher_node,
