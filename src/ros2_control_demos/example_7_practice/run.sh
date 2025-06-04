5313eddba3129f112ad60beac7a62c84e1943226
Chong Liu <chongliu2021@163.com>
2025-06-04 11:03:05 +0800

    优化启动文件名称

--- src/ros2_control_demos/example_7_practice/run.sh
+++ src/ros2_control_demos/example_7_practice/run.sh
@@ -11,8 +11,8 @@ echo "===== 设置环境变量 ====="
 source install/setup.bash
 
 echo "===== 启动 ====="
-# ros2 launch r6bot_control_system view_r6bot.launch.py
+# ros2 launch r6bot_control_system r6bot_view_urdf.launch.py
 # ros2 launch r6bot_control_system r6bot_control_system.launch.py
-ros2 launch r6bot_control_system generate_trajectory.launch.py
+ros2 launch r6bot_control_system r6bot_generate_trajectory.launch.py
 
 echo "===== 结束 ====="
